5fe6188dfa55f29818f3932d698f289b19a5a37d
hexinzxq <73218041+hexinzxq@users.noreply.github.com>
2020-11-04 19:50:19 +0800

    第一天学习Node.js

---
+++ day01/js/helloModule.js
@@ -0,0 +1,13 @@
+// exports.name = 'hexin';
+// exports.age = '23';
+// exports.sayName = function(){
+//     console.log('我是hexin');
+// };
+
+module.exports = {
+    name : 'sdd',
+    age : 12,
+    sayName : function(){
+        console.log('我是ssd');
+    }
+};
